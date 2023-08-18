9316466d15118b6dc18bb8e302f820cb04c6875d
LaryeaNii <zadoklaryea@gmail.com>
2023-08-18 11:01:00 +0000

    another sign of the cross

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,14 +2,31 @@ import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
 import Button from '@/components/button/Button'
+import Location from 'public/location-icon.svg';
+import Mail from 'public/mail-icon.svg';
+import Phone from 'public/phone-icon.svg';
 
 const Contact = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Lets Get in Touch</h1>
+      <div className={styles.contactInfo}>
+        <div className={styles.infoItem}>
+          <Image src={Phone} alt="Phone" className={styles.icon} />
+          <p className={styles.infoText}>123-456-7890</p>
+        </div>
+        <div className={styles.infoItem}>
+          <Image src={Location} alt="Address" className={styles.icon} />
+          <p className={styles.infoText}>123 Main Street, City</p>
+        </div>
+        <div className={styles.infoItem}>
+          <Image src={Mail} alt="Email" className={styles.icon} />
+          <p className={styles.infoText}>info@example.com</p>
+        </div>
+      </div>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
-          <Image src="/contact.png" alt=""
+          <Image src="/contact.png" alt="image icon"
             fill={true}
             className={styles.image}
           />
